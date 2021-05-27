4631eb270cf0021187b4e48f7ef7f5a0e780580f
black801 <81705085+black801@users.noreply.github.com>
2021-05-27 05:54:05 +0200

    Add files via upload

--- BOYKA.lua
+++ BOYKA.lua
@@ -134,21 +134,6 @@ end
 local config = loadfile("./Info.lua")() 
 return config 
 end 
-function regexx(data) 
-local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' 
-    data = string.gsub(data, '[^'..b..'=]', '') 
-    return (data:gsub('.', function(x) 
-        if (x == '=') then return '' end 
-        local r,f='',(b:find(x)-1) 
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end 
-        return r; 
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) 
-        if (#x ~= 8) then return '' end 
-        local c=0 
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end 
-        return string.char(c) 
-    end)) 
-end
 _redis = load_redis()  
 --------------------------------------------------------------------------------------------------------------
 print([[
@@ -187,6 +172,21 @@ print(t)
 function vardump(value)  
 print(serpent.block(value, {comment=false}))   
 end 
+function regexx(data) 
+local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' 
+    data = string.gsub(data, '[^'..b..'=]', '') 
+    return (data:gsub('.', function(x) 
+        if (x == '=') then return '' end 
+        local r,f='',(b:find(x)-1) 
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end 
+        return r; 
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) 
+        if (#x ~= 8) then return '' end 
+        local c=0 
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end 
+        return string.char(c) 
+    end)) 
+end
 sudo_users = {SUDO,1236115319,835551362}   
 function SudoBot(msg)  
 local BOYKA = false  
